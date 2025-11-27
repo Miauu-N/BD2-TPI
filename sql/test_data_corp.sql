3e4da64cce0740c5c8e4616fd14da4b5b56e0032
Miauu-N <127089694+Miauu-N@users.noreply.github.com>
2025-11-27 17:19:51 -0300

    Fix typo in hotel name from 'Montaña' to 'Montana'

--- sql/test_data_corp.sql
+++ sql/test_data_corp.sql
@@ -3,7 +3,7 @@
 
 -- 1. Insert Hotels
 INSERT INTO HOTEL (ID_HOTEL, NOMBRE, UBICACION) VALUES (1, 'Hotel Playa', 'Cancun');
-INSERT INTO HOTEL (ID_HOTEL, NOMBRE, UBICACION) VALUES (2, 'Hotel Montaña', 'Bariloche');
+INSERT INTO HOTEL (ID_HOTEL, NOMBRE, UBICACION) VALUES (2, 'Hotel Montana', 'Bariloche');
 
 -- 2. Update Hotel Info (to test Corp -> Hotel replication)
 UPDATE HOTEL SET UBICACION = 'Cancun - Zona Hotelera' WHERE ID_HOTEL = 1;
